15bbbe5b53c4646dacf8285ee1a8c8384c8e7b30
ProjectCoFi <85388584+ProjectCoFi@users.noreply.github.com>
2021-12-07 04:44:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://glitch.com/edit/#!/bot-justcofi"]
+const urls = ["https://bot-justcofi.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
